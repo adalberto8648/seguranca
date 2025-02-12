f2feda552ad3743b630ba35970504de6f76e43ce
Adalberto <adalberto8648@gmail.com>
2025-02-11 21:32:13 -0300

    nova_senha

--- c_verificacao_senha.py
+++ c_verificacao_senha.py
@@ -30,25 +30,25 @@ def acessar_menu():
 
 def cadastrar_usuario():
     nome = nome_usuario()
-    senha_digitada = senha_escolhida()
+    senha_usuario = senha_digitada()
 
-    while not validar_senha(senha_digitada):
+    while not validar_senha(senha_usuario):
         print("Senha inválida. Tente novamente.")
-        senha_digitada = senha_escolhida()
+        senha_usuario = senha_digitada()
 
     print("----------------------------------")
-    print(f"Senha gerada: {senha_digitada}")
+    print(f"Senha cadastrada pelo usuário: {senha_usuario}")
 
-    caracteres_disponiveis = string.ascii_letters + string.digits + string.punctuation
-    senha_gerada = ''.join(random.choice(caracteres_disponiveis) for numero in range(5))
-    print(f"Senha gerada: {senha_gerada}")
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha_random = ''.join(random.choice(caracteres) for numero in range(5))
+    print(f"5 dígitos gerados por random: {senha_random}")
 
     salt = os.urandom(16)
     print(f"Salt gerado: {salt.hex()}")
-    senhas_com_salt_hex = senha_digitada + senha_gerada + salt.hex()
-    hash_senha = gerar_hash(senhas_com_salt_hex)
+    senhas_com_salt = senha_usuario + senha_random + salt.hex()
+    hash_senha = gerar_hash(senhas_com_salt)
 
-    print(f"Hash SHA-256 da senha: {gerar_hash(senhas_com_salt_hex)}")
+    print(f"Hash SHA-256 da senha: {gerar_hash(senhas_com_salt)}")
     print("----------------------------------\n")
 
     salvar_senha(nome, salt, hash_senha)
@@ -64,16 +64,16 @@ def nome_usuario():
         else:
             return nome
 
-def senha_escolhida():
+def senha_digitada():
     while True:
         print("----------------------------------")
-        senha_digitada = input("Digite aqui a sua senha: ").strip()      
+        senha_usuario = input("Digite aqui a sua senha ou 0 pra sair: ").strip()      
 
-        if senha_digitada == "0":
+        if senha_usuario == "0":
             print("VOCÊ NÃO DIGITOU A SENHA. SAINDO -->")
             exit()
         else:
-            return senha_digitada
+            return senha_usuario
 
 def validar_senha(senha_digitada):
     if len(senha_digitada) < 8:
@@ -89,6 +89,36 @@ def validar_senha(senha_digitada):
         print("A senha precisa ter pelo menos um número.")
         return False
     if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", senha_digitada):
+        print("A senha precisa ter pelo menos um caractere especial.")
+        return False
+    return True
+
+def nova_senha_digitada():
+    while True:
+        nova_senha_usuario = input("Digite a nova senha ou 0 pra sair: ").strip()
+
+        if nova_senha_usuario == "0":
+            print("VOCÊ NÃO DIGITOU A SENHA. SAINDO -->")
+            exit()
+        if not nova_senha_usuario:
+            print("Nenhuma senha informada. Tente novamente.")
+        else:
+            return nova_senha_usuario
+
+def validar_nova_senha(nova_senha_digitada):
+    if len(nova_senha_digitada) < 8:
+        print("A senha precisa ter pelo menos 8 caracteres.")
+        return False
+    if not re.search(r"[A-Z]", nova_senha_digitada):
+        print("A senha precisa ter pelo menos uma letra maiúscula.")
+        return False
+    if not re.search(r"[a-z]", nova_senha_digitada):
+        print("A senha precisa ter pelo menos uma letra minúscula.")
+        return False
+    if not re.search(r"\d", nova_senha_digitada):
+        print("A senha precisa ter pelo menos um número.")
+        return False
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", nova_senha_digitada):
         print("A senha precisa ter pelo menos um caractere especial.")
         return False
     return True
@@ -119,21 +149,22 @@ def alterar_senha():
     for linha in linhas:
         partes = linha.strip().split(":")
         if partes[0] == nome_usuario:
-            nova_senha = input("Digite a nova senha: ").strip()
-            if not nova_senha:
-                print("Nenhuam senha informada. Voltando ao menu.")
-                return
+            nova_senha_usuario = nova_senha_digitada()
             
             print("----------------------------------")
 
-            print(f"Senha gerada: {nova_senha}")
+            print(f"Nova senha digitada: {nova_senha_usuario}")
+
+            while not validar_nova_senha(nova_senha_usuario):
+                print("Senha inválida. Tente novamente.")
+                nova_senha_usuario = nova_senha_digitada()
 
             novos_caracteres_disponiveis = string.ascii_letters + string.digits + string.punctuation
             nova_senha_gerada = ''.join(random.choice(novos_caracteres_disponiveis) for numero in range(5))
-            print(f"Senha gerada: {nova_senha_gerada}")
+            print(f"Senha alterada: {nova_senha_gerada}")
 
             novo_salt = os.urandom(16).hex()
-            novo_hash = hashlib.sha256((nova_senha + nova_senha_gerada + novo_salt).encode()).hexdigest()
+            novo_hash = hashlib.sha256((nova_senha_usuario + nova_senha_gerada + novo_salt).encode()).hexdigest()
 
             novas_linhas.append(f"{nome_usuario}:{novo_salt}:{novo_hash}\n")
             usuario_encontrado = True
