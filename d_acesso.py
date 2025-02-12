2c5cdf4543f553c6db8a0b9b9ddfd385c024bafa
Adalberto <adalberto8648@gmail.com>
2025-02-12 19:06:14 -0300

    alteracao

--- d_acesso.py
+++ d_acesso.py
@@ -4,6 +4,45 @@ import hashlib
 import os
 import re
 
+def acessar_projeto():
+
+    print("\n-------------------------------")
+    print("|           PROJETO           |")
+    print("-------------------------------\n")
+
+    nome = "senha"
+    senha = "123"
+    
+    while True:
+        acesso_nome = input("Digite o nome: ")
+        if acesso_nome == nome:
+            break
+        print("Nome inválido, digite novamente.")
+
+    while True:
+        acesso_senha = input("Digite a senha: ")
+        if acesso_senha == senha:
+            print("Acesso permitido.")
+            break
+        print("Senha inválida, digite novamente.")
+
+    while True:
+        print("\n----- Menu PROJETO -----")
+        print("1 - Clientes")
+        print("2 - Fornecedor")
+        print("3 - Produtos")
+        escolha = input("\nEscolha uma das opções: ")
+        print("-" * 24 + "\n")
+        
+        if escolha == "1":
+            clientes.menu_clientes()
+        elif escolha == "2":
+            fornecedores.menu_fornecedores()
+        elif escolha == "3":
+            produtos.menu_produtos()
+        else:
+            print("Opção inválida, tente novamente\n")
+
 def acessar_menu():
 
     print("\n---------------------------------")
